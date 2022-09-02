161bc689f8974b38c39ade56ce22422cfee05cac
Filip <filipsarovic428@gmail.com>
2022-09-02 18:24:29 +0200

    Made top header section

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
+
+import Me from '../../assets/avatar.jpg'
 import './Header.css'
 
 const Header = () => {
 	return (
-		<div>Header</div>
+		<header>
+			<div className="container header__container">
+				<h4>Hello I am</h4>
+				<h1>Filip Sarovic</h1>
+				<h4 className="text-light">Web Developer</h4>
+				<CTA />
+				<HeaderSocials />
+				{/* <div className="me">
+					<img 
+						src=''
+						alt='me'
+					/>
+				</div> */}
+				<a href='#contact' className='scroll__down'>Scroll Down</a>
+			</div>
+		</header>
 	)
 }
 
